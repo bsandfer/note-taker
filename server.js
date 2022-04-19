0d6a724cf9fe4b815489f151dc40acd4457a3de9
BSandfer <bsandfer@gmail.com>
2022-04-18 19:24:27 -0700

    .

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ app.use(express.json())
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(_dirname, 'public', 'index.html'))
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'notes.html'))
 })
 
 
