b0b0f204ec2bad2cd0615a575c2b15ac4beb152a
BSandfer <bsandfer@gmail.com>
2022-04-18 19:20:46 -0700

    serving html

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const express = require('express')
 
 const app = express()
 
+const path = require('path')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(_dirname, 'public', 'index.html'))
+})
+
+
+
+
 
 
 app.listen(process.env.PORT || 3000)
